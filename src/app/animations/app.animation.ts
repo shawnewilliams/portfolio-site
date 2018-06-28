c9bdf7ebb37f60d5f3f8e205abe55e8b4b0ab090
Shawn Williams <shawn79williams@yahoo.com>
2018-06-28 13:40:33 -0400

    weather app

--- src/app/animations/app.animation.ts
+++ src/app/animations/app.animation.ts
@@ -18,18 +18,18 @@ export function flyInOut() {
     return trigger('flyInOut', [
         state('in', style({transform: 'translateX(0)'})),
         transition('void => *', [
-          animate(300, keyframes([
+          animate(500, keyframes([
             style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
-            style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateX(15px)',  offset: 0.7}),
             style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
-          ]))
+          ])),
         ]),
         transition('* => void', [
-          animate(700, keyframes([
+          animate(500, keyframes([
             style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
             style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
             style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
-          ]))
+          ])), 
         ])
       ])
         
